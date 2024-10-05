a6156be17e2bf0ae2e45971e18877439b7e2f551
Kaï Doh <github@kaidoh.ch>
2024-10-05 08:10:03 +0200

    added max test

--- ft_calloc.c
+++ ft_calloc.c
@@ -16,6 +16,8 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*ptr;
 
+    if (nmemb && size > SIZE_MAX / nmemb)
+		    return (NULL);
 	ptr = malloc(nmemb * size);
 	if (!ptr)
 		return (NULL);
